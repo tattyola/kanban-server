d838172cfe470e266dbdf303e10875dcf8493ae8
Tatsiana A <astrouskaya@yahoo.com>
2025-01-09 22:48:17 -0500

    add statuses servise

--- index.js
+++ index.js
@@ -1,41 +1,75 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
 const bodyParser = require('body-parser')
 const dbConnect = require('./dbConnection')
 const port = 4000
+app.use(cors({
+    origin: port
+}));
 
 dbConnect();
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-let cards = [
-    { id: '1', name: 'first card', status: 'todo', priority: 3 },
-    { id: '2', name: 'second card', status: 'progress', priority: 1 },
-    { id: '3', name: 'third card', status: 'review', priority: 1 },
-    { id: '4', name: 'forth card', status: 'done', priority: 2 },
+let tasks = [
+    { id: '1', name: 'first card', description: 'some desc', status: 'todo', priority: 3 },
+    { id: '2', name: 'second card', description: 'some desc', status: 'progress', priority: 1 },
+    { id: '3', name: 'third card', description: 'some desc', status: 'review', priority: 1 },
+    { id: '4', name: 'forth card', description: 'some desc', status: 'done', priority: 2 },
 ]
+
+let statuses = [
+    {
+        id: '1',
+        title: 'todo',
+        status: 'todo',
+    },
+    {
+        id: '2',
+        title: 'progress',
+        status: 'progress',
+    },
+    {
+        id: '3',
+        title: 'review',
+        status: 'review',
+    },
+    {
+        id: '4',
+        title: 'done',
+        status: 'done',
+    },
+]
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
-app.get('/card', (req, res) => {
-    res.send(cards);
+app.get('/tasks', (req, res) => {
+    res.send(tasks);
 })
 
-app.delete(`/card/:cardId`, (req, res) => {
-    const cardId = req.params.cardId;
-    cards = cards.filter(el => el.id !== cardId);
-    res.send(cards);
+app.get(`/tasks/:taskId`, (req, res) => {
+    const taskId = req.params.taskId;
+    const task = tasks.find(el => el.id === taskId);
+    res.send(task);
 })
 
-app.post('/card', (req, res) => {
+app.delete(`/tasks/:taskId`, (req, res) => {
+    const taskId = req.params.taskId;
+    tasks = tasks.filter(el => el.id !== taskId);
+    res.send(tasks);
+})
+
+app.post('/tasks', (req, res) => {
     const card = req.body;
-    cards.push({
+    tasks.push({
         id: Math.random().toString(),
         ...card
     })
-    res.send('Card has been created')
+    res.send('Task has been created')
 })
 
 // update status
@@ -52,11 +86,15 @@ app.post('/card', (req, res) => {
 // })
 
 // update card
-app.patch('/card/:cardId', (req, res) => {
-    const cardId = req.params.cardId;
-    const updatedCard = req.body;
-    cards = cards.map(el => el.id === cardId ? ({...updatedCard, id: el.id }) : el);
-    res.send('Card has been updated');
+app.patch('/tasks/:taskId', (req, res) => {
+    const taskId = req.params.taskId;
+    const updatedTask = req.body;
+    tasks = tasks.map(el => el.id === taskId ? ({...updatedTask, id: el.id }) : el);
+    res.status(200).send(tasks);
+})
+
+app.get('/statuses', (req, res) => {
+    res.send(statuses);
 })
 
 
